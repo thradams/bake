f04e493b5f43a34cffc3d9be85129244357fed6a
thradams <thiago.adams@gmail.com>
2026-04-13 08:17:23 -0300

    fix

--- test/t2.c
+++ test/t2.c
@@ -1,14 +1,14 @@
 int printf(char *fmt, ...);
 int main() {
-    printf("%d\\n", 5 &gt; 3);
-    printf("%d\\n", 5 &lt; 3);
-    printf("%d\\n", 5 &gt;= 5);
-    printf("%d\\n", 5 &lt;= 4);
-    printf("%d\\n", 5 == 5);
-    printf("%d\\n", 5 != 3);
-    printf("%d\\n", 1 &amp;&amp; 1);
-    printf("%d\\n", 1 &amp;&amp; 0);
-    printf("%d\\n", 0 || 1);
-    printf("%d\\n", 0 || 0);
+    printf("%d\n", 5 > 3);
+    printf("%d\n", 5 < 3);
+    printf("%d\n", 5 >= 5);
+    printf("%d\n", 5 <= 4);
+    printf("%d\n", 5 == 5);
+    printf("%d\n", 5 != 3);
+    printf("%d\n", 1 && 1);
+    printf("%d\n", 1 && 0);
+    printf("%d\n", 0 || 1);
+    printf("%d\n", 0 || 0);
     return 0;
 }
